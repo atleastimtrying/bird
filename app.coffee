147452a6c6dbade862fc7ba980d67b0177191e2f
Anders Fisher <atleastimtrying@gmail.com>
2012-02-22 20:22:21 +0000

    trees added

--- app.coffee
+++ app.coffee
@@ -1,26 +1,65 @@
-STRING = "..........\n
-..........\n
-..........\n
-..........\n
-..........\n
-..........\n"
-
 stdin = process.openStdin()
 stdin.setEncoding 'utf8'
+trees = []
+rows = 10
+columns = 20
+char = 
+  x:0
+  y:0
 
-logString = ->
-  console.log "\u001B[2J\u001B[0;0f#{STRING}"
+roundom = (int)-> Math.ceil Math.random()*int
 
-boxThis = (input)->
-  string = ""
-  for [0..40]
-    do ()->
-      string += input[0] for [0..80]
-      string += '\n'
-  string
+populateTrees = ->
+  for whatever in [0..20]
+    trees.push 
+      x: roundom columns
+      y: roundom rows
 
-inputCallback = (input)->
-  STRING = boxThis(input)
-  logString()
+populateTrees()
 
-stdin.on 'data', (input)-> inputCallback input
+drawTree = (tree)-> table[tree.y][tree.x] = "&"
+
+makeTable = ->
+  table = []
+  for y in [0..rows]
+    table[y] = []
+    table[y].push(".") for x in [0..columns] 
+  table
+
+table = makeTable()
+
+logString = (string)->
+  console.log "\u001B[2J\u001B[0;0f#{string}"
+
+logTable = ->
+  string = ''
+  for row in table
+    string += '\n'
+    string += cell for cell in row
+  logString string
+
+moveChar = (input)->
+  modifyChar input
+  edgeDetection()
+
+
+edgeDetection = ->
+  char.x = 0 if char.x > columns
+  char.x = columns if char.x < 0
+  char.y = 0 if char.y > rows
+  char.y = rows if char.y < 0
+
+modifyChar = (input)->
+  char.y -= 1 if input is "w" and table[char.y - 1][char.x] isnt "&"
+  char.y += 1 if input is "s" and table[char.y + 1][char.x] isnt "&"
+  char.x -= 1 if input is "a" and table[char.y][char.x - 1] isnt "&"
+  char.x += 1 if input is "d" and table[char.y][char.x + 1] isnt "&"
+
+render = (input)->
+  drawTree tree for tree in trees
+  table[char.y][char.x] = "."
+  moveChar input[0]
+  table[char.y][char.x] = "@"
+  logTable()
+
+stdin.on 'data', (input)-> render(input)
--- app.js
+++ app.js
@@ -1,39 +1,104 @@
 (function() {
-  var STRING, boxThis, inputCallback, logString, stdin;
-
-  STRING = "..........\n..........\n..........\n..........\n..........\n..........\n";
-
+  var char, columns, drawTree, edgeDetection, logString, logTable, makeTable, modifyChar, moveChar, populateTrees, render, roundom, rows, stdin, table, trees;
   stdin = process.openStdin();
-
   stdin.setEncoding('utf8');
-
-  logString = function() {
-    return console.log("\u001B[2J\u001B[0;0f" + STRING);
+  trees = [];
+  rows = 10;
+  columns = 20;
+  char = {
+    x: 0,
+    y: 0
   };
-
-  boxThis = function(input) {
-    var string, _fn, _i;
-    string = "";
-    _fn = function() {
-      var _j;
-      for (_j = 0; _j <= 80; _j++) {
-        string += input[0];
-      }
-      return string += '\n';
-    };
-    for (_i = 0; _i <= 40; _i++) {
-      _fn();
+  roundom = function(int) {
+    return Math.ceil(Math.random() * int);
+  };
+  populateTrees = function() {
+    var whatever, _results;
+    _results = [];
+    for (whatever = 0; whatever <= 20; whatever++) {
+      _results.push(trees.push({
+        x: roundom(columns),
+        y: roundom(rows)
+      }));
     }
-    return string;
+    return _results;
   };
-
-  inputCallback = function(input) {
-    STRING = boxThis(input);
-    return logString();
+  populateTrees();
+  drawTree = function(tree) {
+    return table[tree.y][tree.x] = "&";
   };
-
+  makeTable = function() {
+    var table, x, y;
+    table = [];
+    for (y = 0; 0 <= rows ? y <= rows : y >= rows; 0 <= rows ? y++ : y--) {
+      table[y] = [];
+      for (x = 0; 0 <= columns ? x <= columns : x >= columns; 0 <= columns ? x++ : x--) {
+        table[y].push(".");
+      }
+    }
+    return table;
+  };
+  table = makeTable();
+  logString = function(string) {
+    return console.log("\u001B[2J\u001B[0;0f" + string);
+  };
+  logTable = function() {
+    var cell, row, string, _i, _j, _len, _len2;
+    string = '';
+    for (_i = 0, _len = table.length; _i < _len; _i++) {
+      row = table[_i];
+      string += '\n';
+      for (_j = 0, _len2 = row.length; _j < _len2; _j++) {
+        cell = row[_j];
+        string += cell;
+      }
+    }
+    return logString(string);
+  };
+  moveChar = function(input) {
+    modifyChar(input);
+    return edgeDetection();
+  };
+  edgeDetection = function() {
+    if (char.x > columns) {
+      char.x = 0;
+    }
+    if (char.x < 0) {
+      char.x = columns;
+    }
+    if (char.y > rows) {
+      char.y = 0;
+    }
+    if (char.y < 0) {
+      return char.y = rows;
+    }
+  };
+  modifyChar = function(input) {
+    if (input === "w" && table[char.y - 1][char.x] !== "&") {
+      char.y -= 1;
+    }
+    if (input === "s" && table[char.y + 1][char.x] !== "&") {
+      char.y += 1;
+    }
+    if (input === "a" && table[char.y][char.x - 1] !== "&") {
+      char.x -= 1;
+    }
+    if (input === "d" && table[char.y][char.x + 1] !== "&") {
+      return char.x += 1;
+    }
+  };
+  render = function(input) {
+    var tree, _i, _len;
+    for (_i = 0, _len = trees.length; _i < _len; _i++) {
+      tree = trees[_i];
+      drawTree(tree);
+    }
+    table[char.y][char.x] = ".";
+    moveChar(input[0]);
+    table[char.y][char.x] = "@";
+    return logTable();
+  };
   stdin.on('data', function(input) {
-    return inputCallback(input);
+    return render(input);
   });
-
 }).call(this);
